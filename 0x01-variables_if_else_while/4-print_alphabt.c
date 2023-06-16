8802105e5c45f41693ba49de9756e542ae8c82ff
James <arizonahjames@gmail.com>
2023-06-16 02:08:50 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description:a program that prints the alphabet in lowercase, followed by a new line
- * Return: 0
- */
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	char c = 'a';
+	char c;
 
-	while (c <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 		}
-		c++;
 	}
 
 	putchar('\n');
+
 	return (0);
 }
